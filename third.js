50a5bc78a3e3dad334593a29f819912d7ea1d99d
Yash Raj Singh <yashrajsingh7586@gmail.com>
2025-07-19 15:37:48 +0530

    Update third.js

--- third.js
+++ third.js
@@ -206,14 +206,4 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 });
 // footer
-// Optional JS for dropdown language select
-document.getElementById("languageSelect").addEventListener("change", function () {
-  alert("Language changed to: " + this.value);
-});
-//header
-const hamburger = document.getElementById('hamburger');
-const navbar = document.getElementById('navbar');
 
-hamburger.addEventListener('click', () => {
-  navbar.classList.toggle('active');
-});
